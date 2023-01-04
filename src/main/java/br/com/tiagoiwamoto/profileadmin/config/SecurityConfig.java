1218cdcb63faa39e150ed4fc776243c8aef8c835
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2023-01-04 07:36:27 -0300

    Adicionando camada de security

---
+++ src/main/java/br/com/tiagoiwamoto/profileadmin/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.tiagoiwamoto.profileadmin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authz -> authz
+                        .antMatchers("/files/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .cors().and()
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "*"));
+        configuration.setAllowedMethods(List.of("POST", "GET", "PUT", "DELETE", "OPTIONS", "*"));
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(false);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
